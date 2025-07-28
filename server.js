1b029fcbbabad36eae491a3dc5c0674de17caab9
Ha Van Nguyen <nguyen902993@gmail.com>
2025-07-28 19:50:27 +0700

    first commit

--- server.js
+++ server.js
@@ -5,13 +5,31 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(cors({
-  origin: '*', 
+  origin: '*',
   methods: ['GET'],
   allowedHeaders: ['Accept', 'Accept-Version', 'Authorization']
 }));
 app.use(express.json());
 
-const flightRoutes = require('./routes/flights');
+app.get('/', (req, res) => {
+  res.json({
+    message: 'Welcome to Fight Radar API!',
+    version: 'v1',
+    status: 'Server is running',
+    timestamp: new Date().toISOString(),
+    baseUrl: process.env.FR24_BASE_URL || 'https://fr24api.flightradar24.com',
+    documentation: 'https://github.com/havanguyen/fight-radar-api',
+    availableEndpoints: [
+      '/api/static/airlines/:icao/light',
+      '/api/static/airports/:code/light',
+      '/api/static/airports/:code/full',
+      '/api/live/flight-positions/light',
+      '/api/usage'
+    ]
+  });
+});
+
+const flightRoutes = require('./routes/flights'); 
 app.use('/api', flightRoutes);
 
 app.use((err, req, res, next) => {
