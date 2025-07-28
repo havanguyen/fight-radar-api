60ae3c0b5fbd8b88e14ff43bfa0cc3fb67e0ef56
Ha Van Nguyen <nguyen902993@gmail.com>
2025-07-28 19:14:01 +0700

    first commit

--- server.js
+++ server.js
@@ -4,7 +4,12 @@ const cors = require('cors');
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.use(cors());
+const cors = require('cors');
+app.use(cors({
+  origin: '*', 
+  methods: ['GET'],
+  allowedHeaders: ['Accept', 'Accept-Version', 'Authorization']
+}));
 app.use(express.json());
 
 const flightRoutes = require('./routes/flights');
